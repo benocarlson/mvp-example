4c33de158318d9334360f41fe1a1e1192a61032c
benocarlson <benocarlson@gmail.com>
2021-09-13 14:42:58 -0600

    Add solution code

--- MVPExample/app/src/main/java/edu/byu/cs/mvpexample/presenter/MainPresenter.java
+++ MVPExample/app/src/main/java/edu/byu/cs/mvpexample/presenter/MainPresenter.java
@@ -1,7 +1,54 @@
 package edu.byu.cs.mvpexample.presenter;
 
-public class MainPresenter {
-    public interface MainView {
+import edu.byu.cs.mvpexample.model.WordStorage;
 
+public class MainPresenter implements WordStorage.Observer {
+    public interface MainView {
+        void displayError();
+        void showLastWord(String lastWord);
+        String getInputString();
+        void clearInput();
+        void enableInput();
+        void disableInput();
+    }
+
+    private MainView view;
+    private WordStorage wordStorage;
+
+    public MainPresenter(MainView view) {
+        this.view = view;
+        this.wordStorage = WordStorage.getInstance();
+        wordStorage.register(this);
+    }
+    public void verifyText() {
+        String input = view.getInputString();
+        if (input == null || input.length() < 1) {
+            view.disableInput();
+        }
+        else {
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == 'e') {
+                    view.disableInput();
+                    return;
+                }
+            }
+            view.enableInput();
+        }
+    }
+
+    public void enterInput() {
+        String input = view.getInputString();
+        wordStorage.addWord(input);
+        view.clearInput();
+
+    }
+
+    @Override
+    public void update() {
+        if (wordStorage.isErrorState()) {
+            view.displayError();
+        } else {
+            view.showLastWord("Last Word: " + wordStorage.getLastWord());
+        }
     }
 }
--- MVPExample/app/src/main/java/edu/byu/cs/mvpexample/view/MainActivity.java
+++ MVPExample/app/src/main/java/edu/byu/cs/mvpexample/view/MainActivity.java
@@ -3,14 +3,87 @@ package edu.byu.cs.mvpexample.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.byu.cs.mvpexample.R;
+import edu.byu.cs.mvpexample.presenter.MainPresenter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainPresenter.MainView{
+
+    private EditText textInput;
+    private TextView lastWord;
+    private Button submitButton;
+    private MainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textInput = findViewById(R.id.inputText);
+        this.lastWord = findViewById(R.id.lastWord);
+        this.submitButton = findViewById(R.id.submitButton);
+
+        this.presenter = new MainPresenter(this);
+
+        textInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                presenter.verifyText();
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.enterInput();
+            }
+        });
+    }
+
+    @Override
+    public void displayError() {
+        Toast.makeText(MainActivity.this, "Word could not be added!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showLastWord(String lastWord) {
+        this.lastWord.setText(lastWord);
+    }
+
+    @Override
+    public String getInputString() {
+        return textInput.getText().toString();
+    }
+
+    @Override
+    public void enableInput() {
+        submitButton.setEnabled(true);
+    }
+
+    @Override
+    public void disableInput() {
+        submitButton.setEnabled(false);
+    }
+
+    @Override
+    public void clearInput() {
+        textInput.setText("");
     }
 }
