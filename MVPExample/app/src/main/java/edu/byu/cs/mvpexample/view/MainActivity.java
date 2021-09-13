56290b104fb8a80c33d33f3f1374aaee07def09e
benocarlson <benocarlson@gmail.com>
2021-09-13 15:06:52 -0600

    Change a thing

--- MVPExample/app/src/main/java/edu/byu/cs/mvpexample/view/MainActivity.java
+++ MVPExample/app/src/main/java/edu/byu/cs/mvpexample/view/MainActivity.java
@@ -3,14 +3,26 @@ package edu.byu.cs.mvpexample.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import edu.byu.cs.mvpexample.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private EditText textInput;
+    private TextView lastWord;
+    private Button submitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textInput = findViewById(R.id.inputText);
+        this.lastWord = findViewById(R.id.lastWord);
+        this.submitButton = findViewById(R.id.submitButton);
     }
 }
