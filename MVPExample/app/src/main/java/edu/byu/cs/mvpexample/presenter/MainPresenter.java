e1565dfc157c150c730033751956628b208ed6d2
benocarlson <benocarlson@gmail.com>
2021-09-17 16:37:18 -0600

    Partial solution

--- MVPExample/app/src/main/java/edu/byu/cs/mvpexample/presenter/MainPresenter.java
+++ MVPExample/app/src/main/java/edu/byu/cs/mvpexample/presenter/MainPresenter.java
@@ -1,7 +1,31 @@
 package edu.byu.cs.mvpexample.presenter;
 
 public class MainPresenter {
-    public interface MainView {
 
+    private MainView view;
+
+    public interface MainView {
+        String getStringInput();
+        void invalidInput();
+        void validInput();
+    }
+
+    public MainPresenter(MainView view) {
+        this.view = view;
+    }
+
+    public void validateInput() {
+        String input = view.getStringInput();
+        if (input != null && input.length() > 0) {
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == 'e') {
+                    view.invalidInput();
+                    return;
+                }
+            }
+            view.validInput();
+        } else {
+            view.invalidInput();
+        }
     }
 }
--- MVPExample/app/src/main/java/edu/byu/cs/mvpexample/view/MainActivity.java
+++ MVPExample/app/src/main/java/edu/byu/cs/mvpexample/view/MainActivity.java
@@ -3,18 +3,25 @@ package edu.byu.cs.mvpexample.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import edu.byu.cs.mvpexample.R;
+import edu.byu.cs.mvpexample.presenter.MainPresenter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainPresenter.MainView {
 
 
     private EditText textInput;
     private TextView lastWord;
     private Button submitButton;
+
+    private MainPresenter presenter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,38 @@ public class MainActivity extends AppCompatActivity {
         this.textInput = findViewById(R.id.inputText);
         this.lastWord = findViewById(R.id.lastWord);
         this.submitButton = findViewById(R.id.submitButton);
+
+        this.presenter = new MainPresenter(this);
+
+        textInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                presenter.validateInput();
+            }
+        });
+    }
+    @Override
+    public String getStringInput() {
+        return textInput.getText().toString();
+    }
+
+    @Override
+    public void invalidInput() {
+        submitButton.setEnabled(false);
+    }
+
+    @Override
+    public void validInput() {
+        submitButton.setEnabled(true);
     }
 }
